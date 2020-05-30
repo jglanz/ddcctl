a9572ba722cc16d393dc97a0917cc5db89181484
Jonathan Glanz <jonathan.glanz@gmail.com>
2020-05-29 22:57:38 -0400

    - properly implemented DDC controls using actual `CoreGraphics` display list and completely ignoring `NSScreen` - the reason for the changes is that in a Multi-Monitor setup with identical screens, only the first screen is matched due to the use of both `NSScreen` and `CGDisplay`.
    In the original code they were psuedo linked via matching productId, vendorId & serialNumber - but that isn't going to be unique with multiple, identical displays.

---
+++ src/ddcctl-new.cpp
@@ -0,0 +1,81 @@
+
+
+#include "DDC.h"
+#include <cxxopts.hpp>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+
+
+using std::pair;
+using std::make_pair;
+using std::vector;
+using std::string;
+using std::map;
+
+using DDCAction = std::pair<int,int>;
+using Display = DisplayInfo;
+using Displays = vector<Display>;
+
+static void executeDDCAction(const Displays & displays, const DDCAction & action) {
+
+//    command.control_id = static_cast<UInt8>(action.first);
+//    command.new_value = static_cast<UInt8>(action.second);
+
+
+    for (const auto & display : displays) {
+      struct DDCWriteCommand command {
+        static_cast<UInt8>(action.first),
+        static_cast<UInt8>(action.second)
+      };
+
+      printf("D: setting VCP control #%u => %u\n", command.control_id, command.new_value);
+
+      if (!DDCWrite(display, &command)) {
+        printf("E: Failed to send DDC command!\n");
+      }
+    }
+}
+
+int main(int argc, char **argv) {
+  auto allDisplays = getDisplays();
+  auto displays = getDisplays();
+
+  vector<DDCAction> actions;
+
+  cxxopts::Options options("ddcctl", "DDC/CI Monitor Control");
+  options.add_options("common")
+    ("d,display", "Display number", cxxopts::value<int>()->default_value("-1"))
+    ("b,brightness", "Display number", cxxopts::value<int>());
+
+  auto opts = options.parse(argc, argv);
+
+  auto index = opts["display"].as<int>();
+  if (index > -1) {
+    displays.clear();
+    displays.push_back(allDisplays[index]);
+  }
+
+
+  if (opts.count("brightness")) {
+    actions.emplace_back(BRIGHTNESS, opts["brightness"].as<int>());
+//    auto brightness = ;
+
+  }
+  for (const auto &info : displays) {
+    printf("Screen #%u: %s\n", info.index, info.name.data());
+
+
+  }
+
+  for (const auto & action : actions) {
+    executeDDCAction(displays, action);
+  }
+
+
+
+
+
+  return 0;
+}
